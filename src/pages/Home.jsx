2e76c0d05dc4ff694e47c1edd0d65771acb03c73
Ivan Colque <ivan_colque_etn@hotmail.com>
2023-12-21 22:22:24 -0400

    subiendo parrafo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ export function Home() {
         <>
         <h1 className="text-green-500 text-5xl">Home</h1>
         <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus facilis eius iure quia quam amet delectus ducimus. Omnis fuga, blanditiis amet, vitae hic aut laborum neque ex aspernatur nemo quaerat.</h2>
+        <p>parrafo de ivan</p>
         </>
     )
 }
