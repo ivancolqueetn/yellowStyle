3c8b0c64ccde37111a17ddbb52b1bdaa912ebb41
Ivan Colque <ivan_colque_etn@hotmail.com>
2023-12-21 21:41:44 -0400

    actualizando el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 export function Home() {
     return (
+        <>
         <h1 className="text-green-500 text-5xl">Home</h1>
+        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus facilis eius iure quia quam amet delectus ducimus. Omnis fuga, blanditiis amet, vitae hic aut laborum neque ex aspernatur nemo quaerat.</h2>
+        </>
     )
 }
